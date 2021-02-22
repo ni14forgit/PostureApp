1341e7ebb67c0cddcad642e289e39cb902a21779
Nishant <ni14@duke.edu>
2021-02-22 11:25:52 -0500

    fix storyboard issue

---
+++ PostureApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostureApp
+//
+//  Created by Nishant Iyengar on 2/22/21.
+//  Copyright © 2021 Nishant Iyengar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
