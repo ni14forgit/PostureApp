68f5d935d4b63b3e6699a3938a14d1635c5d9be5
Nishant <ni14@duke.edu>
2021-03-24 23:27:35 -0400

    hardware

---
+++ PostureApp/TestHardwareViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TestHardwareViewController.swift
+//  PostureApp
+//
+//  Created by Nishant Iyengar on 3/24/21.
+//  Copyright © 2021 Nishant Iyengar. All rights reserved.
+//
+
+import UIKit
+
+class TestHardwareViewController: UIViewController {
+    
+    @IBOutlet weak var hardwareButton: UIButton?
+    @IBOutlet weak var hardwareLabel: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
